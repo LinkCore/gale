240ca4ac8b30ab6d8d67cb0a555222d0e6a6b073
theEffectOfABrainTumor <90643699+theEffectOfABrainTumor@users.noreply.github.com>
2022-06-02 17:18:57 +0300

    weather logic

--- lib/gale_app.dart
+++ lib/gale_app.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gale/scenes/weather_bloc/weather_bloc.dart';
+
+import 'home_page.dart';
 
 class GaleApp extends StatelessWidget {
   const GaleApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return BlocProvider(
+        create: (context) => WeatherBloc()..add(WeatherStartupEvent()),
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomePage(),
+      ),
+    );
   }
 }
--- lib/scenes/weather_bloc/weather_event.dart
+++ lib/scenes/weather_bloc/weather_event.dart
@@ -2,3 +2,11 @@ part of 'weather_bloc.dart';
 
 @immutable
 abstract class WeatherEvent {}
+
+class WeatherStartupEvent extends WeatherEvent{}
+
+class WeatherCityEvent extends WeatherEvent{
+  final String city;
+
+  WeatherCityEvent({required this.city});
+}
