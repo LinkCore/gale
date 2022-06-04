492adb93c8a6955acd2f414f28a9bb22024d5928
theEffectOfABrainTumor <90643699+theEffectOfABrainTumor@users.noreply.github.com>
2022-06-04 03:01:24 +0300

    home page logic

---
+++ lib/scenes/widgets/weather_initial_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WeatherInitialWidget extends StatelessWidget {
+  const WeatherInitialWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
